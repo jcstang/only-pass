98b2f540797f0b362335c9a09e8d1148558fbe19
Jacob Stanger <jstangwork@gmail.com>
2020-06-23 12:52:06 -0600

    password function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PasswordContainer from './components/PasswordContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <nav className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>OnlyPass</h1>
+        <h6 className="text-muted">The only password generator you will ever need.</h6>
+      </nav>
+      <PasswordContainer />
     </div>
   );
 }
---
+++ src/components/PasswordContainer.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './PasswordContainer.css';
+
+export default function PasswordContainer() {
+
+
+  const [passwordText, setPasswordText] = useState('');
+
+  const generateRandomPassword = () => {
+    const listOfSamplePasswords = [
+      'ght$$jjlkRC##2yK',
+      'ght$$jjlkRC@#$$%^2yK',
+      'yup33t$$jjlkRC##2yK'
+    ];
+
+    return listOfSamplePasswords[0];
+  }
+
+  // const changeHandler = (event) => {
+  //   setPasswordText(event.target.value);
+  // }
+  const changeHandler = (event) => setPasswordText(event.target.value);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    setPasswordText(generateRandomPassword());
+
+  }
+
+  return (
+    <div className="container" id="password-container">
+      <form onSubmit={submitHandler} className="form-inline justify-content-center">
+        <div className="form-group mx-sm-3 mb-2">
+          <label htmlFor="gen-password" className="sr-only">Password</label>
+          <input onChange={changeHandler} type="text" className="form-control" id="gen-password" value={passwordText} placeholder="Password" />
+        </div>
+        <button type="submit" className="btn btn-primary mb-2">Generate Password</button>
+        <button type="button" class="btn btn-warning mb-2 ml-2">Copy</button>
+      </form>
+    </div>
+  );
+}
