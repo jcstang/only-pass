f27bc84e241a83ea9267239a238c831e1e2393f9
Jacob Stanger <jstangwork@gmail.com>
2020-06-23 12:57:00 -0600

    push button random password appears

--- src/components/PasswordContainer.js
+++ src/components/PasswordContainer.js
@@ -3,8 +3,15 @@ import './PasswordContainer.css';
 
 export default function PasswordContainer() {
 
-
+  // USE STATE
+  // =============================================================
   const [passwordText, setPasswordText] = useState('');
+
+
+  const getRandomInt = (maxRange) => {
+    // maxRange = 3, expected output 0, 1, 2
+    return Math.floor(Math.random() * Math.floor(maxRange));
+  }
 
   const generateRandomPassword = () => {
     const listOfSamplePasswords = [
@@ -13,12 +20,9 @@ export default function PasswordContainer() {
       'yup33t$$jjlkRC##2yK'
     ];
 
-    return listOfSamplePasswords[0];
+    return listOfSamplePasswords[getRandomInt(3)];
   }
 
-  // const changeHandler = (event) => {
-  //   setPasswordText(event.target.value);
-  // }
   const changeHandler = (event) => setPasswordText(event.target.value);
 
   const submitHandler = (event) => {
@@ -28,6 +32,8 @@ export default function PasswordContainer() {
 
   }
 
+  // JSX - return statement
+  // =============================================================
   return (
     <div className="container" id="password-container">
       <form onSubmit={submitHandler} className="form-inline justify-content-center">
@@ -36,7 +42,7 @@ export default function PasswordContainer() {
           <input onChange={changeHandler} type="text" className="form-control" id="gen-password" value={passwordText} placeholder="Password" />
         </div>
         <button type="submit" className="btn btn-primary mb-2">Generate Password</button>
-        <button type="button" class="btn btn-warning mb-2 ml-2">Copy</button>
+        <button type="button" className="btn btn-warning mb-2 ml-2">Copy</button>
       </form>
     </div>
   );
