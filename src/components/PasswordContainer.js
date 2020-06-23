13f201013e128eb53cc1c55f9102c49e6ed72d53
Jacob Stanger <jstangwork@gmail.com>
2020-06-23 17:33:04 -0600

    loading copy npm package

--- src/components/PasswordContainer.js
+++ src/components/PasswordContainer.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './PasswordContainer.css';
 import helperFuncs from '../utils/helperFuncs';
+import copy from 'copy-to-clipboard';
 
 export default function PasswordContainer() {
 
@@ -29,6 +30,19 @@ export default function PasswordContainer() {
     setCharacterLength(passwordLength);
   }
 
+  const copyButtonHandler = (event) => {
+    // let copyValue = document.getElementById("copy-button");
+    // copyValue.select();
+    // copyValue.setSelectionRange(0, 99999);
+    // document.execCommand("copy");
+    let copyThing = document.getElementById('copy-button').value;
+
+    copy(document.getElementById('copy-button').value);
+
+    // alert('copied text: ' + copyValue.value);
+
+  }
+
   // JSX - return statement
   // =============================================================
   return (
@@ -48,7 +62,7 @@ export default function PasswordContainer() {
           <input onChange={changeHandler} type="text" className="form-control" id="gen-password" value={passwordText} placeholder="Password" />
         </div>
         <button type="submit" className="btn btn-primary mb-2">Generate Password</button>
-        <button type="button" className="btn btn-warning mb-2 ml-2">Copy</button>
+        <button type="button" className="btn btn-warning mb-2 ml-2" id="copy-button" onClick={copyButtonHandler}>Copy</button>
       </form>
     </div>
   );
